6036ad5aa9d75d981128211a4f8ce6c3831ddb79
coni2k <coni2k@users.noreply.github.com>
2020-07-13 20:35:42 +0300

    - Validation errors - Logo & icon

--- src/app/registration.component.ts
+++ src/app/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './password-validator';
 
 @Component({
   selector: 'app-registration',
@@ -8,12 +9,28 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegistrationComponent {
   registrationForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+    firstName: ['', [Validators.required, Validators.maxLength(255)]],
+    lastName: ['', [Validators.required, Validators.maxLength(255)]],
+    email: ['', [Validators.required, Validators.maxLength(255), Validators.email]],
+    password: ['', [Validators.required, forbiddenNameValidator(/bob/i)]],
     confirmPassword: ['', Validators.required],
   });
+
+  get email(): AbstractControl {
+    return this.registrationForm.get('email');
+  }
+
+  get firstName(): AbstractControl {
+    return this.registrationForm.get('firstName');
+  }
+
+  get lastName(): AbstractControl {
+    return this.registrationForm.get('lastName');
+  }
+
+  get password(): AbstractControl {
+    return this.registrationForm.get('password');
+  }
 
   constructor(private fb: FormBuilder) {}
 
