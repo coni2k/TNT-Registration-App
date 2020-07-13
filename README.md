9586f817bdc30b8e662573c6c678502a1c87a9da
coni2k <coni2k@users.noreply.github.com>
2020-07-13 14:43:03 +0300

    App name rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TNTLoginApp
+# TNTRegistrationApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'TNT-Login-App';
+  title = 'TNT-Registration-App';
 }
