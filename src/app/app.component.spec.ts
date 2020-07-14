ddb5c59ea0f108db01426ed91c2bad3373cfadff
coni2k <coni2k@users.noreply.github.com>
2020-07-14 15:05:05 +0300

    - Refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,10 @@
 import { async, TestBed } from '@angular/core/testing';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration.component';
-import { RegistrationService } from './registration.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        NoopAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-      ],
-      declarations: [AppComponent, RegistrationComponent],
-      providers: [
-        FormBuilder,
-        {
-          provide: RegistrationService,
-          useValue: {},
-        },
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
--- src/app/registration-completed/registration-completed.component.spec.ts
+++ src/app/registration-completed/registration-completed.component.spec.ts
@@ -1,9 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationCompletedComponent } from './registration-completed.component';
 
 describe('RegistrationCompletedComponent', () => {
@@ -12,14 +7,6 @@ describe('RegistrationCompletedComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        NoopAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-      ],
       declarations: [RegistrationCompletedComponent],
     })
       .compileComponents()
