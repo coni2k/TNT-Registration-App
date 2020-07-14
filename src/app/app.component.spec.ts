3607e329922d275816af95c8b56de239c9669f8a
coni2k <coni2k@users.noreply.github.com>
2020-07-14 15:12:02 +0300

    Minor fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, TestBed } from '@angular/core/testing';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [AppRoutingModule],
     }).compileComponents();
   }));
 
